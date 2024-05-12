41c9ec0fc539b187b3f949e894fbb32d18eba351
Silas H. Rasch <silasrasch@gmail.com>
2024-05-12 16:28:05 +0200

    omg

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const router = createRouter({
     {
       path: '/admin-confirm',
       name: 'admin-confirm',
-      component: () => HomeView
+      component: HomeView
     },
     {
       path: '/admin-confirm/:id',
