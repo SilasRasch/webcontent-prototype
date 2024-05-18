4f5adcd41db95388b76df309b4906a66d8f05665
Silas H. Rasch <silasrasch@gmail.com>
2024-05-18 20:54:15 +0200

    Axios base config

--- src/store/api/orderApi.js
+++ src/store/api/orderApi.js
@@ -3,51 +3,64 @@ import { auth } from "../auth"
 import axios from "axios"
 
 const baseURL = "http://192.168.100.201:8282/api/orders"
-var config = { baseURL: baseURL, method: 'get', url: '' }
+var baseConfig = { baseURL: baseURL, method: 'get', url: '' }
 
 export const orderApi = {
     getOrders: async () => {
-        config.method = 'get'
-        config.url = !auth.isAdmin() ? `?userId=${auth.loggedInUser.id}` : '/'
+        const config = {
+            ...baseConfig,
+            method: 'get',
+            url: !auth.isAdmin() ? `?userId=${auth.loggedInUser.id}` : '/'
+        }
 
         const res = await axios(config).catch(err => console.error(err))
         const data = await res.data
         return data
     },
     getOrder: async (id) => {
-        config.method = 'get'
-        config.url = `/${id}`
+        const config = {
+            ...baseConfig,
+            method: 'get',
+            url: `/${id}`
+        }
 
         const res = await axios(config).catch(err => console.error(err))
         return await res.data
     },
     putOrder: async (id, order) => {
-        config.method = 'put'
-        config.url = toString(id)
+        const config = {
+            ...baseConfig,
+            method: 'put',
+            url: `/${id}`,
+            data: order,
+        }
 
-        const uri = '/' + id
-        await axios.put(baseURL + uri, order)
+        await axios(config)
             .then(response => {
                 return response
             })
             .catch(error => console.error(error))
     },
     postOrder: async (order) => {
-        config.method = 'post'
-        config.url = ''
+        const config = {
+            ...baseConfig,
+            method: 'post',
+            data: order,
+        }
 
-        await axios.post(baseURL, order)
+        await axios(config)
             .then(res => {
                 return res
             })
             .catch(error => console.error(error))
     },
     deleteOrder: async (id) => {
-        config.method = 'delete'
-        config.url = toString(id)
-
-        const uri = '/' + id
-        await axios.delete(baseURL + uri)
+        const config = {
+            ...baseConfig,
+            method: 'delete',
+            url: toString(id),
+        }
+        await axios(config)
             .then(res => {
                 return res
             })
