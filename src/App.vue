ad729a8e3d4d94add0bb3b5a7f9f289e071af451
Silas H. Rasch <silasrasch@gmail.com>
2024-06-10 22:35:47 +0200

    new nginx config test

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,14 @@ import { auth } from './store/auth';
       <img alt="logo" src="@/assets/webcontent.png" width="256" />
     </RouterLink>
     
-
-      <div class="flex sm:[&>*]:p-4 [&>*]:p-2 font-semibold items-center text-red-500 text-lg">
-        <RouterLink to="/bestil" v-if="auth.isUser()">Bestil</RouterLink>
-        <RouterLink to="/admin" v-if="auth.isAdmin()">Dashboard</RouterLink>
-        <RouterLink to="/min-side" v-if="auth.isUser() || auth.isCreator()">Min Side</RouterLink>
-        <!-- <ChatModal v-if="auth.isLoggedIn" /> -->
-        <UserModal v-if="auth.isLoggedIn" />
-        <a href="https://webcontent.dk" target="_blank"><img class="max-h-[28px]" src="https://webcontent.dk/wp-content/uploads/2024/01/cropped-logo-hjemmeside-1-32x32.png"></a>
-      </div>
+    <div class="flex sm:[&>*]:p-4 [&>*]:p-2 font-semibold items-center text-red-500 text-lg">
+      <RouterLink to="/bestil" v-if="auth.isUser()">Bestil</RouterLink>
+      <RouterLink to="/admin" v-if="auth.isAdmin()">Dashboard</RouterLink>
+      <RouterLink to="/min-side" v-if="auth.isUser() || auth.isCreator()">Min Side</RouterLink>
+      <!-- <ChatModal v-if="auth.isLoggedIn" /> -->
+      <UserModal v-if="auth.isLoggedIn" />
+      <a href="https://webcontent.dk" target="_blank"><img class="max-h-[28px]" src="https://webcontent.dk/wp-content/uploads/2024/01/cropped-logo-hjemmeside-1-32x32.png"></a>
+    </div>
   </header>
 
   <!-- <RouterView /> -->
