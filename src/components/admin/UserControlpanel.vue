faefc197d30c8fd81bd92a2e938e0d7c573fd9b2
Silas H. Rasch <silasrasch@gmail.com>
2024-06-11 01:18:38 +0200

    Create user redesign

--- src/components/admin/UserControlpanel.vue
+++ src/components/admin/UserControlpanel.vue
@@ -1,39 +1,25 @@
 <script setup>
 import DoubleInput from '@/components/Input/DoubleInput.vue';
 import SingleSelect from '@/components/Input/SingleSelect.vue';
-import CreatorModal from '@/components/modals/CreatorModal/CreatorModal.vue';
 import { useAuthAPI } from '@/store/api/authApi';
 import { useCreatorAPI } from '@/store/api/creatorApi';
 import { ref } from 'vue';
+import UserPool from './UserPool.vue';
 
 const api = useCreatorAPI()
 const authAPI = useAuthAPI()
 const creators = ref(null)
-api.getCreators().then((data) => creators.value = data)
 
+const fetchCreators = () => {
+    api.getCreators().then((data) => creators.value = data).then((data) => { return data })
+}
+
+fetchCreators()
 const showCreate = ref(false)
-
-const showUsers = ref(false)
-const showAdmins = ref(false)
-const showCreators = ref(true)
-
-const toggleCreate = () => {
-    showCreate.value = true
-}
-
-const toggleUsers = () => {
-    showCreate.value = false
-}
-
-const handleDelete = (id) => {
-    console.log(`Delete ${id}`);
-}
-
+const currTab = ref(1)
 const user = ref({ displayName: '', email: '', role: '', password: 'WebContentGenerate' })
-
 const creatorProfile = ref({ location: '', speciality: '', handles: { instagram: '', tikTok: '', facebook: '', youTube: '', snapchat: '', pinterest: '' } })
 
-// Password skal på sigt genereres i backenden, og brugeren vil få en mail med login-informationer.
 const handleRegister = () => {
     if (user.value.role !== 'Creator') {
         authAPI.register(user.value)
@@ -44,67 +30,37 @@ const handleRegister = () => {
             ...creatorProfile.value
         }
         
-        authAPI.registerCreator(creator)
+        authAPI.registerCreator(creator).then(() => {
+            user.value = { displayName: '', email: '', role: '', password: 'WebContentGenerate' }
+            creatorProfile.value = { location: '', speciality: '', handles: { instagram: '', tikTok: '', facebook: '', youTube: '', snapchat: '', pinterest: '' } }
+            fetchCreators().then(() => {
+                showCreate.value = false
+            })
+        })
     }
 }
 </script>
 
 <template>
     <div class="grid justify-center text-center m-4 mt-0">
-        <div class="grid bg-slate-800 rounded-lg p-2 md:w-[36.5rem]">
-            <div class="flex gap-2">
-                <button v-if="showCreate" class="bg-green-500 w-full rounded-lg p-2 hover:bg-green-600 duration-200" @click="toggleUsers">
-                    Tilbage
-                </button>
-                <button v-else class="bg-green-500 w-full rounded-lg p-2 hover:bg-green-600 duration-200" @click="toggleCreate">
-                    Opret ny bruger
-                </button>
+        <div v-if="!showCreate" class="grid">
+            
+            <div class="flex justify-center rounded-lg pb-1 mb-[-0.5rem] text-lg cursor-pointer">
+                <span class="p-2 px-6 font-semibold rounded-tl-lg hover:bg-red-500" :class="{'bg-red-500': currTab === 1, 'bg-red-400': currTab !== 1}" @click="currTab = 1">Creators</span>
+                <span class="p-2 px-6 font-semibold hover:bg-red-500" :class="{'bg-red-500': currTab === 2, 'bg-red-400': currTab !== 2}" @click="currTab = 2">Brugere</span>
+                <span class="p-2 px-6 font-semibold rounded-tr-lg hover:bg-red-500" :class="{'bg-red-500': currTab === 3, 'bg-red-400': currTab !== 3}" @click="currTab = 3">Admins</span>
             </div>
+            
+            <TransitionGroup tag="div" class="flex justify-center bg-slate-800 rounded-lg [&>*]:max-w-[50rem]" name="dashboard">
+                <UserPool v-if="currTab === 1" v-model="creators">Creators</UserPool>
+                <UserPool v-if="currTab === 2" v-model="creators">Almindelige brugere</UserPool>
+                <UserPool v-if="currTab === 3" v-model="creators">Administratorer</UserPool>
+            </TransitionGroup>
         </div>
 
-        
-        <div v-if="!showCreate" class="grid md:w-[36.5rem]">
-            <div class="grid bg-slate-800 rounded-lg p-2 mt-3">
-                <div class="p-0 m-2 flex justify-between items-center" @click.self="showCreators = !showCreators">
-                    <p class="p-0 m-0 text-left text-lg">Creators</p>
-                    <i v-if="showCreators" class="fa fa-filter text-xl cursor-pointer" @click="showCreators = false"></i>
-                    <i v-else class="fa fa-angle-down text-xl cursor-pointer" @click="showCreators = true"></i>
-                </div>
-                
-                <div v-if="showCreators" class="grid grid-cols-5 justify-center pr-2 max-h-[25rem] scrollbar">
-                    <CreatorModal v-for="creator in creators" :key="creator.id" :creator="creator" show-delete @remove-creator="n => handleDelete(n)" />
-                </div>
-            </div>
-
-            <div class="grid bg-slate-800 rounded-lg p-2 my-3">
-                <div class="p-0 m-2 flex justify-between items-center" @click.self="showUsers = !showUsers">
-                    <p class="p-0 m-0 text-left text-lg">Alm. brugere</p>
-                    <i v-if="showUsers" class="fa fa-filter text-xl cursor-pointer" @click="showUsers = false"></i>
-                    <i v-else class="fa fa-angle-down text-xl cursor-pointer" @click="showUsers = true"></i>
-                </div>
-                
-                <div v-if="showUsers" class="grid justify-center pr-2 max-h-[25rem] scrollbar">
-                    <!-- <CreatorModal v-for="creator in creators" :key="creator.id" :creator="creator" /> -->
-                    <p class="w-full p-2 px-4 bg-slate-900 rounded-lg">Der er desværre ikke noget at vise her</p>
-                </div>
-            </div>
-
-            <div class="grid bg-slate-800 rounded-lg p-2 mb-3">
-                <div class="p-0 m-2 flex justify-between items-center" @click.self="showAdmins = !showAdmins">
-                    <p class="p-0 m-0 text-left text-lg">Admins</p>
-                    <i v-if="showAdmins" class="fa fa-filter text-xl cursor-pointer" @click="showAdmins = false"></i>
-                    <i v-else class="fa fa-angle-down text-xl cursor-pointer" @click="showAdmins = true"></i>
-                </div>
-                
-                <div v-if="showAdmins" class="grid justify-center pr-2 max-h-[25rem] scrollbar">
-                    <!-- <CreatorModal v-for="creator in creators" :key="creator.id" :creator="creator" /> -->
-                     <p class="w-full p-2 px-4 bg-slate-900 rounded-lg">Der er desværre ikke noget at vise her</p>
-                </div>
-            </div>
-        </div>
-
-        <div v-else class="bg-slate-800 rounded-lg p-4 md:w-[36.5rem] mt-3">
+        <div v-else class="bg-slate-800 rounded-lg p-4 md:w-[36.5rem]">
             <h1 class="text-xl">Opret ny bruger</h1>
+            <hr class="my-2 opacity-50">
 
             <DoubleInput v-model:firstInput="user.displayName" v-model:secondInput="user.email" required
             placeholder-one="Navnet på brugeren" placeholder-two="Brugerens email">
@@ -140,8 +96,20 @@ const handleRegister = () => {
             </div>
 
             <hr class="text-black bg-black opacity-50 h-0.5 m-3" />
-            <button class="bg-green-500 w-full hover:bg-green-600 duration-200 p-2 px-4 rounded-lg font-semibold"
-            @click="handleRegister">Opret</button>
+            <div class="flex justify-center"><button class="bg-red-400 w-full hover:bg-red-500 duration-200 p-2 mx-1 rounded-lg font-semibold"
+                @click="handleRegister">Opret</button></div>
+            
+        </div>
+
+        <div class="grid m-2 font-semibold justify-center">
+            <div class="flex gap-2 w-fit">
+                <button v-if="showCreate" class="bg-red-400 w-full rounded-lg p-2 hover:bg-red-500 duration-200" @click="showCreate = false">
+                    Tilbage
+                </button>
+                <button v-else class="bg-red-400 w-full rounded-lg p-2 hover:bg-red-500 duration-200" @click="showCreate = true">
+                    Opret ny bruger
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -151,5 +119,34 @@ const handleRegister = () => {
   overflow-y: scroll;
   scrollbar-color: grey rgba(94, 94, 94, 0.15);
   scrollbar-width: thin;
+}
+
+.dashboard-enter-from {
+  opacity: 0;
+}
+
+.dashboard-enter-to {
+  opacity: 1;
+}
+
+.dashboard-enter-active {
+  transition: all 0.6s ease-in-out;
+}
+
+.dashboard-leave-from {
+  opacity: 1;
+}
+
+.dashboard-leave-to {
+  opacity: 0;
+}
+
+.dashboard-leave-active {
+  transition: all 0.6s ease-in-out;
+  position: absolute;
+}
+
+.dashboard-move {
+  transition: all 0.6s ease-in-out;
 }
 </style>
---
+++ src/components/admin/UserPool.vue
@@ -0,0 +1,28 @@
+<script setup>
+import CreatorModal from '../modals/CreatorModal/CreatorModal.vue';
+
+const model = defineModel()
+
+const handleDelete = (id) => {
+    console.log(`Delete ${id}`);
+}
+</script>
+
+<template>
+    <div class="grid gap-2 w-[90vw] p-2 rounded-xl mt-0">
+        <!-- Column 1 -->
+        <div class="grid gap-2">
+            <h3 class="text-xl font-semibold p-1 m-0 bg-red-500 text-white rounded-xl"><slot></slot></h3>
+            <hr class="text-white bg-white rounded-full opacity-50 h-1 mx-0.5 border-none" />
+
+            <div v-if="model">
+                <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-7 -m-2">
+                    <CreatorModal v-for="creator in model" :key="creator.id" :creator="creator" show-delete @remove-creator="n => handleDelete(n)" />
+                </div>
+            </div>
+            <div v-else>
+                <p class="text-white sm:text-lg font-semibold py-6 bg-slate-800 rounded-lg">Der er ingen brugere at se her...</p>
+            </div>
+        </div>
+    </div>
+</template>
