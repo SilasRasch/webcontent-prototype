f0221ffd8cb83c64af1d6dd1ed63758e20007297
Silas H. Rasch <silasrasch@gmail.com>
2024-06-24 22:27:34 +0200

    email api endpoint

--- src/store/api/emailApi.js
+++ src/store/api/emailApi.js
@@ -2,8 +2,8 @@ import { auth } from "../auth"
 import axios from "axios"
 
 export const useEmailAPI = () => {
-    // const baseURL = "https://api.wcp.dk/data/email" // Public Prod
-    const baseURL = "https://api.nobitches.win/data/email" // Public test
+    const baseURL = "https://api.wcp.dk/data/email" // Public Prod
+    // const baseURL = "https://api.nobitches.win/data/email" // Public test
     const baseConfig = { baseURL: baseURL, method: 'get', url: '', headers: { 'Authorization': auth.token } }
 
     // { userEmail, reportMessage }
